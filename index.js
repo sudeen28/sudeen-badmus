d10cc05fec6e004a4bfbadf0893ed43af2b1e4c2
sudeen28 <polygrammy28@gmail.com>
2023-10-09 09:59:16 +0100

    added a modal

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+// const navbar = 
+//     document.querySelector('.navbar');
+// let lastScrollTop = 0;
+
+// window.addEventListener(
+//     "scroll", 
+//     () => {
+//     var { pageYOffset } = window;
+//     if (pageYOffset > lastScrollTop) {
+//         // downward scroll
+//         navbar.classList.remove("visible");
+//     } else if (pageYOffset < lastScrollTop) {
+//         // upwards scroll
+//         navbar.classList.add("visible");
+//     } 
+//     lastScrollTop = 
+//         pageYOffset <= 0 ? 0 : pageYOffset;
+//     },
+//     { passive: true }
+
+// );
 
