c4ea53d8b73da3d489b93f33800f84544caf9871
sudeen28 <polygrammy28@gmail.com>
2024-01-09 14:10:04 +0100

    commit

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Simulate content loading (replace with your actual content loading logic)
     setTimeout(function () {
-      // Content is loaded, remove the loading state
       document.body.classList.add('loaded');
-    }, 2000); // Adjust the timeout based on your content loading time
+    }, 3000);
   });
